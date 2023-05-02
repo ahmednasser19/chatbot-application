ea19a9c611f77277814a58539bd68d3b641768da
ahmed nasser <ahmednas197@gmail.com>
2023-05-02 21:51:46 +0300

    added send message to the server logic with frontend validation

--- src/app/components/ChatInput.tsx
+++ src/app/components/ChatInput.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { cn } from "@/lib/utils";
+import { Message } from "@/lib/validators/message";
+import { useMutation } from "@tanstack/react-query";
+import { nanoid } from "nanoid";
 import { FunctionComponent, HTMLAttributes, useState } from "react";
 import TextareaAutosize from "react-textarea-autosize";
 interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {}
@@ -10,6 +13,23 @@ const ChatInput: FunctionComponent<ChatInputProps> = ({
 }) => {
   const [input, setInput] = useState<string>("");
 
+  const { mutate: sendMessage, isLoading } = useMutation({
+    mutationFn: async (message: Message) => {
+      const response = await fetch("/api/message", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ message: "Hello" }),
+      });
+
+      return response.body;
+    },
+
+    onSuccess: () => {
+      console.log("Success");
+    },
+  });
   return (
     <div {...props} className={cn("border-t border-zinc-300", className)}>
       <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
@@ -18,6 +38,19 @@ const ChatInput: FunctionComponent<ChatInputProps> = ({
           maxRows={4}
           autoFocus
           value={input}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && !e.shiftKey) {
+              e.preventDefault();
+
+              const message: Message = {
+                id: nanoid(),
+                isUserMessage: true,
+                text: input,
+              };
+
+              sendMessage(message);
+            }
+          }}
           onChange={(e) => setInput(e.target.value)}
           placeholder="Write a message..."
           className="peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-100 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6"
